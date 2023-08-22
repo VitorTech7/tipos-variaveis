7a6c9e648eb79c4faf2284bbd3dde2aa06672a6c
Vitor <vytroxalberto@gmail.com>
2023-08-21 23:42:41 -0300

    teste2

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,5 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá mundo");
+        System.out.println("Olá mundo1");
     }
 }
